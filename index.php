ca001f4530f8255350d832a2102e2126efe54f88
elmerolero <isma.salas@outlook.com>
2020-04-13 03:19:48 -0500

    Lineas de prueba para clase Usuario.php

--- index.php
+++ index.php
@@ -2,8 +2,15 @@
 	require "Clases/Usuario.php";
 
 	$nuevoUsuario = new Usuario(); 	
-	$nuevoUsuario -> establecerNombre( "Juan" );
-	$nuevoUsuario -> establecerApellidos( "Perez Robles" );
-	$nuevoUsuario -> establecerFechaNacimiento( "Perez Robles" );
+	$nuevoUsuario -> establecerNombre( "Ismael" );
+	$nuevoUsuario -> establecerApellidos( "Salas López" );
+	$nuevoUsuario -> establecerFechaNacimiento( "1999-03-11" );
+	$nuevoUsuario -> establecerAvatar( "Avatar" );
+	$nuevoUsuario -> establecerCorreoElectronico( "isma.salas24@gmail.com" );
+	$nuevoUsuario -> establecerUsuario( "elmerolero" );
+	$nuevoUsuario -> establecerContrasena( "dsslot#@" );
+	$nuevoUsuario -> establecerNivel( 0 );
+	$nuevoUsuario -> establecerExperiencia( 0 );
+
 
 	echo $nuevoUsuario -> toString();
