84ce6a2a85a20ae79e3e1730e9466337a3345696
elmerolero <isma.salas@outlook.com>
2020-04-13 03:11:51 -0500

    Se añadieron manipuladores de acceso.

--- Clases/Usuario.php
+++ Clases/Usuario.php
@@ -7,10 +7,11 @@ class Usuario
 	private $apellidos;
 	private $fechaNacimiento;
 	private $avatar;
-	private $nivel;
-	private $experiencia;
+	private $correoElectronico;
 	private $usuario;
 	private $contrasena;
+	private $nivel;
+	private $experiencia;
 
 
 	/* Métdos */
@@ -29,12 +30,93 @@ class Usuario
 		$this -> fechaNacimiento = $fechaNacimiento;
 	}
 
+	function establecerAvatar( $fotoAvatar )
+	{
+		$this -> avatar = $fotoAvatar;
+	}
+
+	function establecerCorreoElectronico( $correoElectronico )
+	{
+		$this -> correoElectronico = $correoElectronico;
+	}
+
+	function establecerUsuario()
+	{
+		$this -> usuario = $usuario;
+	}
+
+	function establecerContrasena( $contrasena )
+	{
+		$this -> contrasena = $contrasena;
+	}
+
+	function establecerNivel( $nivel )
+	{
+		$this -> nivel = $nivel;
+	}
+
+	function establecerExperiencia( $experiencia )
+	{
+		$this -> experiencia = $experiencia;
+	}
+
+	// Getters
+	function obtenerNombre()
+	{
+		return $this -> nombre;
+	}
+
+	function obtenerApellidos()
+	{
+		return $this -> apellidos;
+	}
+
+	function obtenerFechaNacimiento()
+	{
+		return $this -> fechaNacimiento;
+	}
+
+	function obtenerAvatar()
+	{
+		return $this -> avatar;
+	}
+
+	function obtenerCorreoElectronico()
+	{
+		return $this -> correoElectronico;
+	}
+
+	function obtenerUsuario()
+	{
+		return $this -> usuario;
+	}
+
+	function obtenerContrasena()
+	{
+		return $this -> contrasena;
+	}
+
+	function obtenerNivel()
+	{
+		return $this -> nivel;
+	}
+
+	function obtenerExperiencia()
+	{
+		return $this -> experiencia;
+	}
 
 
 	function toString()
 	{
-		return "Nombre: " . $this -> nombre . "<br>" .
-			   "Apellidos: " . $this -> apellidos . "<br>" .
-			   "Fecha nacimiento: " . $this -> fechaNacimiento . "<br>";
+		return "Nombre: " . $this -> obtenerNombre() . "<br>" .
+			   "Apellidos: " . $this -> obtenerApellidos() . "<br>" .
+			   "Fecha nacimiento: " . $this -> obtenerFechaNacimiento() . "<br>" .
+			   "Avatar: " . $this -> obtenerAvatar() . "<br>" .
+			   "Correo electrónico: " . $this -> obtenerCorreoElectronico() . "<br>" .
+			   "Usuario: " . $this -> obtenerUsuario() . "<br>" .
+			   "Contraseña: " . $this -> obtenerContrasena() . "<br>" .
+			   "Nivel: " . $this -> obtenerNivel() . "<br>" .
+			   "Experiencia: " . $this -> obtenerExperiencia() . "<br>";
 	}
 }
