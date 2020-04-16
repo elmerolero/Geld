ec57e3ec38703707007b97f1f4bdaf6ac188bc18
elmerolero <isma.salas@outlook.com>
2020-04-16 18:59:26 -0500

    Se añadieron includes para los archivos de configuración.

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
-	require_once "configuracion/Configuracion.php";
-	require_once "configuracion/Database.php";
+	require_once "configuracion/configuracion.php";
+	require_once "configuracion/database.php";
+	require_once "clases/sesion.php";
 	require_once "libs/App.php";
 
 	$app = new App();
