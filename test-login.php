2c58f56e381384e9847a370ccdb1a959cce3660d
elmerolero <isma.salas@outlook.com>
2020-04-16 19:17:09 -0500

    Pagina temporar para el testeo de inicio de sesión.

---
+++ test-login.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form action="/Geld/sesion/iniciarSesion" method="POST">
+		<input type="text" placeholder="Usuario" name="usuario"><br>
+		<input type="password" placeholder="Contraseña" name="contrasena"><br>
+		<input type="submit" value="Enviar"/>
+	</form>
+</body>
